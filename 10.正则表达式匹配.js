e1b72a23a2b6d39915a98cae0f5718e14f6236f0
blitz.whtoo <whtoo@qq.com>
2020-02-11 23:22:56 +0800

    1. 增加dp相关练习

---
+++ 10.正则表达式匹配.js
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode.cn id=10 lang=javascript
+ *
+ * [10] 正则表达式匹配
+ *
+ * https://leetcode-cn.com/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (25.96%)
+ * Likes:    928
+ * Dislikes: 0
+ * Total Accepted:    50.9K
+ * Total Submissions: 196K
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * 
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 
+ * 
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * 
+ * 说明:
+ * 
+ * 
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * s = "aa"
+ * p = "a"
+ * 输出: false
+ * 解释: "a" 无法匹配 "aa" 整个字符串。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * s = "aa"
+ * p = "a*"
+ * 输出: true
+ * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入:
+ * s = "ab"
+ * p = ".*"
+ * 输出: true
+ * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入:
+ * s = "aab"
+ * p = "c*a*b"
+ * 输出: true
+ * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    let memo = {}
+    let dp = function(i,j){
+        if(memo[i+','+j] != undefined) {
+            return memo[i+','+j]
+        }
+
+        if(j == p.length) {
+            return i == s.length
+        }
+
+        let firstMatch = (i < s.length) && (s[i] == p[j] || p[j] == '.')
+        let res = 0
+        if(j <= p.length - 2 && p[j+1] == '*') {
+            res = dp(i,j+2) || (firstMatch && dp(i+1,j))
+        } else {
+            res = firstMatch && dp(i + 1,j+1)
+        }
+        memo[i+','+j] = res
+
+        return res
+    }
+
+    return dp(0,0)
+};
+// @lc code=end
+
---
+++ 303.区域和检索-数组不可变.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (59.52%)
+ * Likes:    131
+ * Dislikes: 0
+ * Total Accepted:    27.8K
+ * Total Submissions: 46.7K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n' +
+  '[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 
+ * 示例：
+ * 
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+
+    this.lst = nums
+    let len  = nums.length
+    this.dp = new Array(len+1)
+    this.dp.fill(0)
+    for (let j = 1; j <= len; j++) {
+        this.dp[j] = this.dp[j-1] + this.lst[j-1]
+    }
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (i, j) {
+    
+    return this.dp[j+1] - this.dp[i]
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
+
+// @lc code=end
