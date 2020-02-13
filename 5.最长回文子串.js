8ee71ff788db9e2e6d3aa3a1d5e0faba8c07ddf5
blitz.whtoo <whtoo@qq.com>
2020-02-13 20:29:56 +0800

    1. DP版本

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -36,7 +36,45 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
+    if(s.length == 0) return ""
+    if(s.length == 1) return s
+    /// 1. 常规 DP 版本
+    let n = s.length
+    /// 2. init dp[n][n] -> i to j
+    let dp = new Array(n)
+    for(let i = 0; i < n;i++) {
+        dp[i] = new Array(n)
+        dp[i].fill(false)
+    }
+    /// 3. 
+    let maxLen = 1
+    let start = 0
+    dp[0][0] = true
+    for(let i = 1; i < n;i++){
+        dp[i][i] = true
+        for(let j=0;j < i;j++){
+            if(s[i] == s[j]){
+                /// j-> a...x...a <- i
+                if(i-j < 3) {
+                    dp[j][i] = true
+                } else {
+                    dp[j][i] = dp[j+1][i-1]
+                }
+            } else {
+                dp[j][i] = false
+            }
+
+            if(dp[j][i]){
+                let curLen = i-j+1
+                if(curLen > maxLen){
+                    maxLen = curLen
+                    start = j
+                }
+            }
+        }
+    }
+    return s.slice(start,start+maxLen)
 };
 // @lc code=end
 
+longestPalindrome('ac')
