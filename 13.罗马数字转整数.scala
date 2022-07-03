6f21af3e76cbf3f0632a78f2c56d09fb528fd6f8
willson.wang <drdangran@gmail.com>
2022-07-03 16:02:50 +0800

    1. add 13 and 14

---
+++ 13.罗马数字转整数.scala
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=13 lang=scala
+ *
+ * [13] 罗马数字转整数
+ */
+
+// @lc code=start
+object Solution {
+    def romanToInt(s: String): Int = {
+        
+    }
+}
+// @lc code=end
+
---
+++ 14.最长公共前缀.scala
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=scala
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+object Solution {
+    def longestCommonPrefix(strs: Array[String]): String = {
+        if (strs.length <= 0) return ""
+        var prefix = ""
+        var firstElem = strs[0]
+        var tmp = ""
+        for (s <- firstElem) {
+            tmp += s
+            for (elem <- strs.drop(1)) {
+                if(!elem.startsWith(tmp)) {
+                    return prefix
+                }
+            }
+            prefix = tmp
+        }
+
+        return prefix
+    }
+}
+// @lc code=end
+
